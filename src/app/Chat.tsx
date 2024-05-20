bb52ab6c8749df2bb225e7ea14ed86f24860d5ae
LuisGilGB <luis.gilgb@gmail.com>
2024-05-20 23:50:01 +0200

    Style changes for the chat

--- src/app/Chat.tsx
+++ src/app/Chat.tsx
@@ -29,14 +29,26 @@ const Chat = ({ className, messagesWrapperClassName }: ChatProps) => {
         ref={scrollRef}
       >
         <div
-          className={cn('relative mx-auto max-w-2xl px-4 pb-[200px] pt-4 md:pt-10', messagesWrapperClassName)}
+          className={cn(
+            'relative mx-auto max-w-2xl px-4 pb-[200px] pt-4 flex flex-col gap-y-4 md:pt-10',
+            messagesWrapperClassName,
+          )}
           ref={messagesRef}
         >
-          {conversation.map((message: ClientMessage) => (
-            <div key={message.id}>
-              {message.role}: {message.display}
-            </div>
-          ))}
+          {conversation.map((message: ClientMessage) =>
+            message.role === 'user' ? (
+              <div
+                key={message.id}
+                className="bg-amber-200 rounded-b-xl rounded-tl-xl text-right self-right px-4 py-2 ml-auto"
+              >
+                {message.display}
+              </div>
+            ) : (
+              <div key={message.id} className="bg-sky-200 rounded-b-xl rounded-tr-xl px-4 py-2 mr-auto">
+                {message.display}
+              </div>
+            ),
+          )}
         </div>
         <div className="fixed inset-x-0 bottom-0 w-full py-4 bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
           <div className="mx-auto sm:max-w-2xl sm:px-4">
