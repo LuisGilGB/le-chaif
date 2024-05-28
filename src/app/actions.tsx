4c3c390193a04b3707e930d723a053d65629ed7f
LuisGilGB <luis.gilgb@gmail.com>
2024-05-28 23:29:46 +0200

    Improved the prompt used to get a recipe from the AI model

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,13 +1,13 @@
 'use server';
 
-import { BotMessage } from '@/components/Message';
+import {BotMessage} from '@/components/Message';
 import RecipeCard from '@/components/recipe/RecipeCard';
-import { recipeSchema } from '@/schemas/recipe.schema';
-import { openai } from '@ai-sdk/openai';
-import { createAI, getMutableAIState, streamUI } from 'ai/rsc';
-import { nanoid } from 'nanoid';
-import { ReactNode } from 'react';
-import { z } from 'zod';
+import {recipeSchema} from '@/schemas/recipe.schema';
+import {openai} from '@ai-sdk/openai';
+import {createAI, getMutableAIState, streamUI} from 'ai/rsc';
+import {nanoid} from 'nanoid';
+import {ReactNode} from 'react';
+import {z} from 'zod';
 
 export interface ServerMessage {
   role: 'user' | 'assistant';
@@ -43,7 +43,7 @@ export async function continueConversation(input: string): Promise<ClientMessage
     },
     tools: {
       getRecipe: {
-        description: 'Return a recipe with its ingredients, instructions, image, required toold and more',
+        description: 'Return a recipe with its ingredients, preparation steps, image, required tools and more. Search the web to provide valid links for the recipe image, the ingredients images and the shop links. Preparation steps must not be preceded by a number. All the ingredients must include a quantity expressed in units, weight or any other metric. Be as descriptive as possible with the preparation steps, assuming the reader doesn\'t have knowledge about different cooking techniques. Be exhaustive enumerating the required tools, no matter how trivial it can be (in example: if a wooden spoon is useful, enumerate the wooden spoon). The recipe must include a video link from the web if available.',
         parameters: z.object({
           recipe: recipeSchema,
         }),
