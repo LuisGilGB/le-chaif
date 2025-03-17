695ecdf0aed80943a9ec3187d2b9f59ad70ccbcb
LuisGilGB <luis.gilgb@gmail.com>
2025-03-17 23:03:11 +0100

    Installed React 19 and Next 15.2

--- src/lib/hooks/useEnterSubmit.ts
+++ src/lib/hooks/useEnterSubmit.ts
@@ -1,10 +1,10 @@
 import { type RefObject, useRef } from 'react';
 
 export const useEnterSubmit = (): {
-  formRef: RefObject<HTMLFormElement>;
+  formRef: RefObject<HTMLFormElement | null>;
   onKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
 } => {
-  const formRef = useRef<HTMLFormElement>(null);
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>): void => {
     if (event.key === 'Enter' && !event.shiftKey && !event.nativeEvent.isComposing) {
